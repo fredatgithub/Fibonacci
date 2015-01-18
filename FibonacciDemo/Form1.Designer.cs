f836bd6521939692c520c68fb1b796225821b3e6
Freddy Juhel <domaine75@gmail.com>
2015-01-18 20:17:27 +0100

    inital commit

---
+++ FibonacciDemo/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace FibonacciDemo
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.listBox1 = new System.Windows.Forms.ListBox();
+      this.buttonCalculate = new System.Windows.Forms.Button();
+      this.progressBar1 = new System.Windows.Forms.ProgressBar();
+      this.label1 = new System.Windows.Forms.Label();
+      this.listBox2 = new System.Windows.Forms.ListBox();
+      this.SuspendLayout();
+      // 
+      // listBox1
+      // 
+      this.listBox1.FormattingEnabled = true;
+      this.listBox1.Location = new System.Drawing.Point(35, 75);
+      this.listBox1.Name = "listBox1";
+      this.listBox1.Size = new System.Drawing.Size(111, 641);
+      this.listBox1.TabIndex = 0;
+      // 
+      // buttonCalculate
+      // 
+      this.buttonCalculate.Location = new System.Drawing.Point(370, 75);
+      this.buttonCalculate.Name = "buttonCalculate";
+      this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
+      this.buttonCalculate.TabIndex = 1;
+      this.buttonCalculate.Text = "Calculate";
+      this.buttonCalculate.UseVisualStyleBackColor = true;
+      this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+      // 
+      // progressBar1
+      // 
+      this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+      this.progressBar1.Location = new System.Drawing.Point(0, 722);
+      this.progressBar1.Name = "progressBar1";
+      this.progressBar1.Size = new System.Drawing.Size(519, 23);
+      this.progressBar1.TabIndex = 2;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(77, 23);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(35, 13);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "label1";
+      // 
+      // listBox2
+      // 
+      this.listBox2.FormattingEnabled = true;
+      this.listBox2.Location = new System.Drawing.Point(184, 75);
+      this.listBox2.Name = "listBox2";
+      this.listBox2.Size = new System.Drawing.Size(111, 641);
+      this.listBox2.TabIndex = 4;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(519, 745);
+      this.Controls.Add(this.listBox2);
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.progressBar1);
+      this.Controls.Add(this.buttonCalculate);
+      this.Controls.Add(this.listBox1);
+      this.Name = "Form1";
+      this.Text = "How to calculate a Fibonacci number";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.ListBox listBox1;
+    private System.Windows.Forms.Button buttonCalculate;
+    private System.Windows.Forms.ProgressBar progressBar1;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.ListBox listBox2;
+  }
+}
+
---
+++ FibonacciDemo/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FibonacciDemo
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    Int64[] FiboList = new long[100];
+    private int compteurFiboList = 0;
+
+    private Int64 Fibonacci(Int64 number)
+    {
+      if (number <= 2)
+      {
+        return 1;
+      }
+
+      return Fibonacci(number - 1) + Fibonacci(number - 2);
+    }
+
+    private Int64 GetFibonacci(Int64 number)
+    {
+      if (FiboList.Contains(number))
+      {
+        return FiboList[number];
+      }
+
+      compteurFiboList++;
+      FiboList[compteurFiboList] = Fibonacci(number);
+      return FiboList[compteurFiboList];
+    }
+
+    private void buttonCalculate_Click(object sender, EventArgs e)
+    {
+      const int mini = 1;
+      const int maxi = 47;
+      progressBar1.Minimum = mini;
+      progressBar1.Maximum = maxi;
+      Stopwatch chrono = new Stopwatch();
+      progressBar1.Value = progressBar1.Minimum;
+      listBox1.Items.Clear();
+      chrono.Start();
+      for (Int64 i = mini; i < maxi; i++)
+      {
+        listBox1.Items.Add(i + " = " + Fibonacci(i));
+        listBox2.Items.Add(GetFibonacci(i));
+        Application.DoEvents();
+        progressBar1.Value = (int)i;
+      }
+
+      chrono.Stop();
+      label1.Text = "Duration is: " + chrono.ElapsedMilliseconds;
+
+      progressBar1.Value = progressBar1.Minimum;
+    }
+  }
+}
